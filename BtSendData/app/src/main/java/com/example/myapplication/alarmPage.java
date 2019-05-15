891762123c6baa7d0ba20ac522373b50a4ecc8f6
DetailsMatterM <DetailsMatterM@users.noreply.github.com>
2019-05-15 16:41:41 +0200

    implementation of a timecontrolled function to send a signal to the arduino

--- BtSendData/app/src/main/java/com/example/myapplication/alarmPage.java
+++ BtSendData/app/src/main/java/com/example/myapplication/alarmPage.java
@@ -7,11 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextClock;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -23,7 +23,9 @@ public class alarmPage extends AppCompatActivity {
     private Button btnActivate;
     private ImageButton returnButton;
     private TimePicker alarmTime;
-    private TextClock currentTime;
+
+    SimpleDateFormat simpleDateFormat;
+    Calendar calender;
 
     private final static String TAG = "";
 
@@ -43,7 +45,6 @@ public class alarmPage extends AppCompatActivity {
             }
         });
 
-
         btnActivate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,45 +56,55 @@ public class alarmPage extends AppCompatActivity {
             }
         });
 
+        //alarm time refers to the time-picker, it is set to a 24 hour format
         alarmTime = findViewById(R.id.timePicker);
-        currentTime = findViewById(R.id.textClock);
+        alarmTime.setIs24HourView(true);
 
         Timer t = new Timer();
+        //this method is used to schedule an event
         t.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                if (currentTime.getText().toString().equals(AlarmTime())) {
+                calender = Calendar.getInstance();
+                simpleDateFormat = new SimpleDateFormat("HH:mm");
 
+                //this method compares if the current time is the same like the time set on the time picker, implementation of the button "set alarm" would be possible by
+                //the use of a boolean
+                if (simpleDateFormat.format(calender.getTime()).equals(AlarmTime())) {
+                    //toastMessage("activated");
+                    String activateMode = "f";
+                    byte[] bytes = activateMode.getBytes(Charset.defaultCharset());
+                    write(bytes);
+                    Log.d(TAG, "Button f pressed");
                 }
             }
+            //delay = when does it start comparing, period = how often does it compare, 1000 ms = 1 sec
         }, 0, 1000);
     }
     public String AlarmTime(){
-        Calendar rightNow = Calendar.getInstance();
-        //this is 12 hour format, change to 24 hour format is possible
-        int alarmHours = rightNow.get(Calendar.HOUR_OF_DAY);
-        int alarmMinutes = rightNow.get(Calendar.MINUTE);
+
+        int alarmHours = alarmTime.getHour();
+        int alarmMinutes = alarmTime.getMinute();
 
         String stringAlarmTime;
+        String stringAlarmMinutes;
 
-
-        if(alarmHours > 12){
-            alarmHours = alarmHours - 12;
-            stringAlarmTime = Integer.toString(alarmHours).concat(":").concat(Integer.toString(alarmMinutes).concat(("PM")));
+        if(alarmMinutes < 10) {
+            stringAlarmMinutes = "0";
+            stringAlarmMinutes = stringAlarmMinutes.concat(Integer.toString(alarmMinutes));
         } else {
-            stringAlarmTime = Integer.toString(alarmHours).concat(":").concat(Integer.toString(alarmMinutes).concat("AM"));
+            stringAlarmMinutes = Integer.toString(alarmMinutes);
         }
 
-        return stringAlarmTime;
+        stringAlarmTime = Integer.toString(alarmHours).concat(":").concat(stringAlarmMinutes);
 
+        return stringAlarmTime;
     }
 
 
 
     private void toastMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-
-
     }
 
 }
